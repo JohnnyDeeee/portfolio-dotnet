f786753ed92857246404263ac6c7a188cbfce655
JohnnyDeeee <gamefake@live.nl>
2025-10-20 23:16:27 +0200

    Show projects from db in home page
    Also added a link to the project's page

---
+++ Portfolio/Data/Project.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Server.Data;
+
+public class Project {
+    public int Id { get; set; }
+    [MaxLength(250)] public required string Title { get; set; }
+    // ReSharper disable once EntityFramework.ModelValidation.UnlimitedStringLength (We allow long descriptions)
+    public required string Description { get; set; }
+}
