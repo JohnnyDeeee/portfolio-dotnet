62a2fdbafc13e18b1818499844cbe793e5daa836
JohnnyDeeee <gamefake@live.nl>
2025-10-20 23:15:18 +0200

    Fix issue where hot reload didnt work
    Resulted in error "Attribute @rendermode is only valid when used on a component."

--- Portfolio.Server/Data/Project.cs
+++
@@ -1,7 +0,0 @@
-namespace Portfolio.Server.Data;
-
-public class Project {
-    public int Id { get; set; }
-    public required string Title { get; set; }
-    public required string Description { get; set; }
-}
