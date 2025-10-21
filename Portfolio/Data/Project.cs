acfa637623797794bb584185ee7b28ef241c0472
JohnnyDeeee <gamefake@live.nl>
2025-10-21 16:38:49 +0200

    Limit description length
    We don't want to show the full description on the home page

--- Portfolio/Data/Project.cs
+++ Portfolio/Data/Project.cs
@@ -4,7 +4,7 @@ namespace Portfolio.Server.Data;
 
 public class Project {
     public int Id { get; set; }
-    [MaxLength(250)] public required string Title { get; set; }
+    [MaxLength(100)] public required string Title { get; set; }
     // ReSharper disable once EntityFramework.ModelValidation.UnlimitedStringLength (We allow long descriptions)
     public required string Description { get; set; }
 }
